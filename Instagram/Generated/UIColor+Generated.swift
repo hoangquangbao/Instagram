cf257a12d49e0bb7fa0160c0167558c6178a9dba
Thuan Tran <quocthuan240495@gmail.com>
2022-09-21 17:09:16 +0700

    view UIMain

--- Instagram/Generated/UIColor+Generated.swift
+++ Instagram/Generated/UIColor+Generated.swift
@@ -4,6 +4,7 @@
 import SwiftUI
 
 extension Color {
+    static var accentColor1: Color { return Color("AccentColor 1") }
     static var accentColor: Color { return Color("AccentColor") }
     static var _262626: Color { return Color("#262626") }
     static var _3797Ef: Color { return Color("#3797EF") }
