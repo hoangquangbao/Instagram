8ddd53e1048920ca224206c8449fbce38dcdcb77
Quang Bao <hoangquangbao.developer@gmail.com>
2022-10-10 13:46:16 +0700

    Keep SignupView frame

--- Instagram/UI/Login/SignUpView.swift
+++ Instagram/UI/Login/SignUpView.swift
@@ -8,53 +8,55 @@ struct SignUpView: View {
     @State private var _selectedIndex: Int = 1
     
     var body: some View {
-        VStack(spacing: 20) {
-        
-            backButton()
-            Group {
-                Text(vm.headerTitle)
-                    .font(.sfProTextBold(25, relativeTo: .largeTitle))
-                
-                SegmentedPickerView(
-                    titles: vm.pickerTitles,
-                    selectedIndex: $_selectedIndex)
-                
-                if _selectedIndex == 0 {
-                    PhoneTextFieldView()
-                } else {
-                    TextField(vm.emailTitle, text: $vm.email)
-                        .textFieldStyle(CustomTextFieldStyle())
-                }
-                
-                Button {
-                } label: {
-                    Text(vm.nextButtonTitle)
-                }
-                .buttonStyle(CustomButtonStyle())
-                .opacity(vm.email.isEmpty||_selectedIndex == 0 ? 0.5 : 1)
-                .disabled(vm.email.isEmpty||_selectedIndex == 0)
-                
-                if _selectedIndex == 0 {
-                    Text(vm.phoneOptionDescription)
-                        .font(.sfProTextRegular(13, relativeTo: .title1))
-                        .foregroundColor(Color.black.opacity(0.5))
-                        .multilineTextAlignment(.center)
-                        .padding(.horizontal, 30)
-                        .fixedSize(horizontal: false, vertical: true)
-                }
-                
-                Spacer()
-            }
-            .padding(.horizontal, 30)
+        NavigationView {
+            VStack(spacing: 20) {
             
-            BottomBarView(questionText: vm.questionText,actionText: vm.actionText) {
-                withAnimation {
-                    vmLogin.isShowSignUpView = false
+                backButton()
+                Group {
+                    Text(vm.headerTitle)
+                        .font(.sfProTextBold(25, relativeTo: .largeTitle))
+                    
+                    SegmentedPickerView(
+                        titles: vm.pickerTitles,
+                        selectedIndex: $_selectedIndex)
+                    
+                    if _selectedIndex == 0 {
+                        PhoneTextFieldView()
+                    } else {
+                        TextField(vm.emailTitle, text: $vm.email)
+                            .textFieldStyle(CustomTextFieldStyle())
+                    }
+                    
+                    Button {
+                    } label: {
+                        Text(vm.nextButtonTitle)
+                    }
+                    .buttonStyle(CustomButtonStyle())
+                    .opacity(vm.email.isEmpty||_selectedIndex == 0 ? 0.5 : 1)
+                    .disabled(vm.email.isEmpty||_selectedIndex == 0)
+                    
+                    if _selectedIndex == 0 {
+                        Text(vm.phoneOptionDescription)
+                            .font(.sfProTextRegular(13, relativeTo: .title1))
+                            .foregroundColor(Color.black.opacity(0.5))
+                            .multilineTextAlignment(.center)
+                            .padding(.horizontal, 30)
+                            .fixedSize(horizontal: false, vertical: true)
+                    }
+                    
+                    Spacer()
+                }
+                .padding(.horizontal, 30)
+                
+                BottomBarView(questionText: vm.questionText,actionText: vm.actionText) {
+                    withAnimation {
+                        vmLogin.isShowSignUpView = false
+                    }
                 }
             }
+            .background()
+            .environmentObject(vm)
         }
-        .background()
-        .environmentObject(vm)
     }
 }
 
