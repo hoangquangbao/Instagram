0f2305e4e84ddb9422027deaa2699320a6191e8a
Bao Hoang <hoangquangbao.developer@gmail.com>
2022-10-14 23:50:58 +0700

    Refactor code

--- Instagram/UI/Login/SignupView/SignupInputView.swift
+++ Instagram/UI/Login/SignupView/SignupInputView.swift
@@ -14,80 +14,102 @@ struct SignupInputView: View {
             Text(vm.headerTitle)
                 .font(.sfProTextBold(22, relativeTo: .largeTitle))
             
-            //MARK: add_email screen
             if vm.type == .add_email {
-                SegmentedPickerView(
-                    titles: vm.pickerTitle ?? [],
-                    selectedIndex: $_selectedIndex)
-                
-                if _selectedIndex == 0 {
-                    PhoneTextFieldView()
-                } else {
-                    TextField(vm.textfieldTitle, text: $text)
-                        .textFieldStyle(CustomTextFieldStyle())
-                        .keyboardType(.emailAddress)
-                }
-                
-                Button {
-                    isNavigation = vm.action()
-                } label: {
-                    Text(vm.buttonLable)
-                }
-                .buttonStyle(CustomButtonStyle())
-                .opacity(text.isEmpty||_selectedIndex == 0 ? 0.5 : 1)
-                .disabled(text.isEmpty||_selectedIndex == 0)
-                
-                if _selectedIndex == 0 {
-                    Text(vm.description)
-                        .font(.sfProTextRegular(13, relativeTo: .title1))
-                        .foregroundColor(Color.black.opacity(0.5))
-                        .multilineTextAlignment(.center)
-                        .padding(.horizontal, 30)
-                        .fixedSize(horizontal: false, vertical: true)
-                }
-            }
-            //MARK: other screen
-            else {
-                Text(vm.description)
-                    .font(.sfProTextRegular(15, relativeTo: .caption1))
-                    .foregroundColor(Color.black.opacity(0.6))
-                    .lineSpacing(3)
-                    .multilineTextAlignment(.center)
-                    .fixedSize(horizontal: false, vertical: true)
-                
-                TextField(vm.textfieldTitle, text: $text)
-                    .textFieldStyle(CustomTextFieldStyle())
-                    .keyboardType(vm.type == .add_confirmation_code ? .numberPad : .default)
-                
-                if vm.type == .add_password {
-                    HStack(alignment: .center, spacing: 5) {
-                        Button {
-                            self._isSavePassword.toggle()
-                        } label: {
-                            Image(systemName: _isSavePassword ? "checkmark.square.fill" : "square")
-                                .resizable()
-                                .frame(width: 20, height: 18)
-                        }
-                        
-                        Text(vm.saveTitle ?? "")
-                            .font(.sfProTextRegular(13, relativeTo: .title1))
-                            .foregroundColor(Color.black.opacity(0.6))
-        
-                        Spacer()
-                    }
-                }
-
-                Button {
-                    isNavigation = vm.action()
-                } label: {
-                    Text(vm.buttonLable)
-                }
-                .buttonStyle(CustomButtonStyle())
-                .opacity(text.isEmpty ? 0.5 : 1)
-                .disabled(text.isEmpty)
+                addEmailView()
+            } else {
+                otherView()
             }
             Spacer()
         }
         .padding(.horizontal, 30)
+    }
+}
+
+extension SignupInputView {
+    
+    private func addEmailView() -> some View {
+        VStack(spacing: 20) {
+            SegmentedPickerView(
+                titles: vm.pickerTitle ?? [],
+                selectedIndex: $_selectedIndex)
+            
+            if _selectedIndex == 0 {
+                PhoneTextFieldView()
+            } else {
+                TextField(vm.textfieldTitle, text: $text)
+                    .textFieldStyle(CustomTextFieldStyle())
+                    .keyboardType(.emailAddress)
+            }
+            
+            Button {
+                isNavigation = vm.action()
+            } label: {
+                Text(vm.buttonLable)
+            }
+            .buttonStyle(CustomButtonStyle())
+            .opacity(text.isEmpty||_selectedIndex == 0 ? 0.5 : 1)
+            .disabled(text.isEmpty||_selectedIndex == 0)
+            
+            if _selectedIndex == 0 {
+                Text(vm.description)
+                    .font(.sfProTextRegular(13, relativeTo: .title1))
+                    .foregroundColor(Color.black.opacity(0.5))
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 30)
+                    .fixedSize(horizontal: false, vertical: true)
+            }
+        }
+    }
+    
+    private func otherView() -> some View {
+        VStack(spacing: 20) {
+            Text(vm.description)
+                .font(.sfProTextRegular(15, relativeTo: .caption1))
+                .foregroundColor(Color.black.opacity(0.6))
+                .lineSpacing(3)
+                .multilineTextAlignment(.center)
+                .fixedSize(horizontal: false, vertical: true)
+            
+            ZStack {
+                if vm.type == .add_password {
+                    SecureField(vm.textfieldTitle, text: $text)
+                } else {
+                    TextField(vm.textfieldTitle, text: $text)
+                }
+            }
+            .textFieldStyle(CustomTextFieldStyle())
+            .keyboardType(vm.type == .add_confirmation_code ? .numberPad : .default)
+            
+            if vm.type == .add_password {
+                savePasswordView()
+            }
+            
+            Button {
+                isNavigation = vm.action()
+            } label: {
+                Text(vm.buttonLable)
+            }
+            .buttonStyle(CustomButtonStyle())
+            .opacity(text.isEmpty ? 0.5 : 1)
+            .disabled(text.isEmpty)
+        }
+    }
+    
+    private func savePasswordView() -> some View {
+        HStack(alignment: .center, spacing: 5) {
+            Button {
+                self._isSavePassword.toggle()
+            } label: {
+                Image(systemName: _isSavePassword ? "checkmark.square.fill" : "square")
+                    .resizable()
+                    .frame(width: 20, height: 18)
+            }
+            
+            Text(vm.saveTitle ?? "")
+                .font(.sfProTextRegular(13, relativeTo: .title1))
+                .foregroundColor(Color.black.opacity(0.6))
+            
+            Spacer()
+        }
     }
 }
